fad9c5ca0b630e734ce0aa9e320fec7f397cee52
depahomov <pahomov_dmitrii@mail.ru>
2023-09-26 12:01:04 +0700

    решена задача № 6

--- Program.cs
+++ Program.cs
@@ -35,6 +35,7 @@ else
 22 3 9 -> 22
 */
 
+/*
 Console.Write("Введите первое число (a): ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число (b): ");
@@ -53,4 +54,23 @@ else
     {
         Console.Write($"Числа {a} и {b} равны");
     }
+}
+*/
+
+/*
+Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+Console.Write("Введите целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine($"Число {a} чётное");
+}
+else
+{
+    Console.WriteLine($"Число {a} нечётное");
 }
